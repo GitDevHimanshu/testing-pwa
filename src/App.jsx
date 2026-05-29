6971635313eab53986fc7e1cf9c2e71cfd0ec7d9
himanshu <himanshu11199@gmail.com>
2026-05-29 10:33:18 +0530

    fetch request sent for caching

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((resp) => resp.json())
+    .then((data) => setData(data))
+  }, [])
   return (
     <div>
-      <h1>this is akashdeep's pwa</h1>
+      {data.map((val)=>{
+        return (
+          <div style={{
+            border: "1px solid black",
+            margin: "20px"
+          }}>
+            <p>name: {val.name}</p>
+            <p>email: {val.email}</p>          
+          </div>
+        )
+      })}
     </div>
   )
 }
